c54420407b64e35264c12de06d985616feb4ddf0
fanus abraha <fanus.abraha@yahoo.com>
2024-05-29 11:52:52 -0500

    creating classes

---
+++ src/main/java/coderscampus/assignment14/controller/UserController.java
@@ -0,0 +1,4 @@
+package coderscampus.assignment14.controller;
+
+public class UserController {
+}
---
+++ src/main/java/coderscampus/assignment14/service/UserService.java
@@ -0,0 +1,7 @@
+package coderscampus.assignment14.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
